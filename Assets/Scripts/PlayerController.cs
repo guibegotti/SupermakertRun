19d84e4be68adad29ad361d0999ce0e527dc29e3
guibegotti <domingos.guilherme@gmail.com>
2018-04-10 22:32:29 -0300

    Adding project to github

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour {
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Vertical");
+        //float moveVertical = Input.GetAxis ("Horizontal");
+
+        //Vector3 movement = new Vector3 (moveHorizontal, 0.0f, -(moveVertical));
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        //rigidbody.velocity = movement;
+        rigidbody.AddForce(moveHorizontal, 0, 0, ForceMode.Acceleration);
+
+    }
+
+}
